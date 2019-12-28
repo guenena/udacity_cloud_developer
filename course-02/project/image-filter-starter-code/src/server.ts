141991d9ff969465ec3ce3a904047f1f378c211a
Omar Guenena <omar.guenena@li.me>
2019-12-28 13:42:17 -0800

    implement TODO 1

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const URL = require("url").URL;
+
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+const isValidURL = (s: string): boolean => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
 
 (async () => {
 
@@ -9,28 +20,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage?", async ( req, res ) => {
+    const {image_url} = req.query;
+    if (image_url == null) {
+      res.status(400).send('Missing \'image_url\' param');
+      return;
+    }
+    if (!isValidURL(image_url)) {
+      res.status(400).send(`Invalid 'image_url' param '${image_url}'`);
+      return;
+    }
+    const filtered_image = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filtered_image, async function(err) {
+      if (err) {
+        res.status(500).send('Failed to send file!');
+      } else {
+        await deleteLocalFiles([filtered_image]);
+      }
+    });
+  } );
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
